caed4ddbe109a5663743deab34883bccefb1983f
jbouwman7 <jbouwman7@st.noorderpoort.nl>
2023-03-28 10:06:29 +0200

    lil update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       center: true,
     },
     extend: {
+      colors: {
+        'primary': '#FFBB29',
+        'secondary': '#FCE055',
+        'contrast': '#0898CC',
+        'light': '#FAF6EE',
+        'dark': '#130A0A',
+      },
     },
   },
   plugins: [],
