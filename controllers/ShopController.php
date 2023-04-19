f8518fbad4af814d214bf6edb95475bec1c83e14
jbouwman7 <jbouwman7@st.noorderpoort.nl>
2023-04-19 11:03:34 +0200

    'nav-bar'

--- controllers/ShopController.php
+++ controllers/ShopController.php
@@ -4,12 +4,10 @@ class ShopController extends BaseController
 {
     public function index()
     {
-        $categories = R::findAll('categories');
         $items = R::findAll('shopitems');
         $users = R::findAll('users');
         showTemplate('shops/index.twig', [
             "users" => $users,
-            "categories" => $categories,
             "items" => $items,
         ]);
     }
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+const body = document.querySelector("body");
+const user_logo = document.querySelector("#user");
+const user_list = document.querySelector(".user-list");
+
+user_logo.addEventListener("click", () => {
+    user_list.classList.toggle("hidden");
+});
+
+
