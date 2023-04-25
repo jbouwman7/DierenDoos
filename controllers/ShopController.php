8666ed425f1e8ec4326913647987bd50005fc306
Kyra Moesker <kmoesker1@st.noorderpoort.nl>
2023-04-25 14:24:46 +0200

    Latest changes to showing shopping cart items

--- controllers/ShopController.php
+++ controllers/ShopController.php
@@ -4,7 +4,6 @@ use RedBeanPHP\Util\Dump;
 
 class ShopController extends BaseController
 {
-
     public function index()
     {
 
@@ -14,18 +13,33 @@ class ShopController extends BaseController
     public function shop()
     {
         $items = R::findAll('shopitems');
-        showTemplate('shops/shop.twig',[
+        showTemplate('shops/shop.twig', [
             'items' => $items,
         ]);
     }
-
     public function shopPost()
     {
+        if (isset($_SESSION['user']['id'])) {
             $item = R::dispense('shoppingcart');
             $item->userId = $_SESSION['user']['id'];
             $item->itemId = $_POST['itemId'];
             $item->amount = $_POST['amount'];
             R::store($item);
-            header('../shop/shop');
+        }
+        header('../shop/shop');
+    }
+    public function cart()
+    {
+        $cart = R::findAll('shoppingcart');
+        $i = 0;
+        foreach ($cart as $item) {
+            $name = R::find('shopitems', $item[2]);
+            echo $item[2];
+            $cart[$i][2] = $name;
+            $i++;
+        }
+        showTemplate('shops/cart.twig', [
+            'cart' => $cart,
+        ]);
     }
 }
